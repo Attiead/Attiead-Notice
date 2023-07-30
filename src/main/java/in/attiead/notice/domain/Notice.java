b48e7c937da3fdd26bec5274bad4fcfd073ccae9
sanghoon <nonsleep79@gmail.com>
2023-07-30 17:58:31 +0900

    "chore : reflect feedback - update notice id column name"

--- src/main/java/in/attiead/notice/domain/Notice.java
+++ src/main/java/in/attiead/notice/domain/Notice.java
@@ -9,22 +9,22 @@ import java.util.List;
 
 @Entity
 @SuperBuilder
-@AttributeOverride(name = "id", column = @Column(name = "notice_id", updatable = false))
+@AttributeOverride(name = "id", column = @Column(name = "id", updatable = false))
 @NoArgsConstructor
 @Getter
 public class Notice extends BaseEntity {
 
-    @Column(name = "notice_title", length = 30, nullable = false)
+    @Column(name = "title", length = 30, nullable = false)
     private String title;
 
-    @Column(name = "notice_content", length = 1000, nullable = false)
+    @Column(name = "content", length = 1000, nullable = false)
     private String content;
 
-    @Column(name = "notice_author", length = 10, nullable = false)
+    @Column(name = "author", length = 10, nullable = false)
     private String author;
 
     @OneToMany
-    @Column(name = "notice_attachments")
+    @Column(name = "attachments")
     private List<Attachment> attachments;
 
     @Enumerated(EnumType.STRING)
