cbde362d9068d0da8bfcb8c02629aa36666dbfb3
sanghoon <nonsleep79@gmail.com>
2023-11-02 18:28:27 +0900

    feat : 공지사항 수정 테스트

---
+++ src/test/java/in/attiead/notice/application/service/NoticeUpdateServiceTest.java
@@ -0,0 +1,15 @@
+package in.attiead.notice.application.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class NoticeUpdateServiceTest {
+    // given
+
+    // when
+
+    // then
+
+}
